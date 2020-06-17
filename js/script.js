fe9024a6aee48de42aaf7e84dd39902f369a05f6
unknown <sumit.agrawal93@gmail.com>
2020-06-17 20:27:20 +0530

    Team Section Added

--- js/script.js
+++ js/script.js
@@ -22,6 +22,21 @@ $("#work").magnificPopup({
     
 });
 
+$(function(){
+
+    $("#team-members").owlCarousel({
+        items:3,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoplayHoverPause:true
+
+
+    });
+
+
+})
+
 
 
 
