5226c3e8a3487f12d60bfd8e56369943ad37d827
unknown <sumit.agrawal93@gmail.com>
2020-06-17 19:24:15 +0530

    First Commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function() {
+
+    new WOW().init();
+
+})
+
+
+$(function()
+{
+
+$("#work").magnificPopup({
+
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{
+        enabled:true
+    }
+
+
+});
+
+    
+});
+
+
+
+
