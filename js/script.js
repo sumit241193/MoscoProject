61d8a1df9c8e37c741bb6e1d5c90077788614eb0
unknown <sumit.agrawal93@gmail.com>
2020-06-18 19:50:47 +0530

    Testimonial Section

--- js/script.js
+++ js/script.js
@@ -38,5 +38,21 @@ $(function(){
 })
 
 
+$(function(){
+
+    $("#customers-testimonials").owlCarousel({
+
+        items:1,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoplayHoverPause:true
+
+    });
+
+
+})
+
+
 
 
